548702e6daf8ed578aefcf64abd3c3c80f67ceb1
julian <julian47310@gmail.com>
2020-04-07 19:31:10 +0200

    Update 20a.rb

--- 20a.rb
+++ 20a.rb
@@ -1,12 +1,8 @@
-puts "choisi un nombre entre 1 et 25"
+puts "choisi un nombre"
 nombre = gets.chomp.to_i
 i = 1
 d = "#"
 while (i <= nombre)
-  if (nombre <1 || <25)
-  puts"un chiffre entre 1 et 25!!"
-  nombre = gets.chomp.to_i
-  end
  puts "#{d*i}"
  i += 1
 end
