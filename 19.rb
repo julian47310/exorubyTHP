9119aaf109526851b5b0092187aef71368cfc5ad
julian <julian47310@gmail.com>
2020-04-07 18:58:20 +0200

    Add files via upload

---
+++ 19.rb
@@ -0,0 +1,13 @@
+ i = 1
+mails = []
+while (i <= 50)
+ if (i%2 == 0)
+  puts "julian#{i}@gmail.com"
+ end
+ mails += ["julian#{i}@gmail.com"]
+ i += 1
+end
+ 
+ 
+ 
+ 
---
+++ 4.rb
@@ -0,0 +1,2 @@
+#ça marche pas car il y a juste un guillemet a gauche
+puts "Salut, ça farte ?
