f7a3b4b349bd64516e770fbe79fa704ef4e18086
julian <julian47310@gmail.com>
2020-04-08 10:08:28 +0200

    Update 20a.rb

--- 20a.rb
+++ 20a.rb
@@ -2,7 +2,7 @@ puts "choisi un nombre entre 1 et 25"
 nombre = gets.chomp.to_i
 i = 1
 d = "#"
-if (nombre <1 || >25)
+while (nombre <1 || >25)
  puts "entre 1 et 25!!"
  nombre = gets.chomp.to_i
 end
